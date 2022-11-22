9d8abddfe953e362edbd2ecfe64e1fa817aac9d9
Leandro Ruiz <leandroruiz5398@gmail.com>
2022-11-22 17:51:13 -0300

    resolvi el problema que tenia con el about

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'blog/home.html')
 
-def about(about):
+def about(request):
     return render(request, 'blog/about.html')
 # Create your views here.
