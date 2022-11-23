a3a511880094c1e5199d471083b61d020efd6e81
Leandro Ruiz <leandroruiz5398@gmail.com>
2022-11-23 13:30:30 -0300

    parte 7, implemente login y logout

---
+++ users/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+
+
+class UserRegisterForm(UserCreationForm):
+    email=forms.EmailField(required=False) #no quiero que el mail sea obligatorio
+    competicion1=forms.CharField()         #este es obligatorio, el usuario tiene que anotarse a una competencia al menos
+    competicion2=forms.CharField(required=False) #este no es obligatorio, si se quiere anotar a dos competencias puede hacerlo, pero no es obligatorio 
+
+    class Meta:
+        model=User
+        fields = ['username', 'email','competicion1','competicion2', 'password1','password2']
+
+
+
+
+
+
+
+
+
+
+
+
+
--- users/views.py
+++ users/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
 def register(request):
     if request.method =='POST':
-        form = UserCreationForm()
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
-            messages.succes(request, f'Usuario {username} creado con exito!')
-            return redirect ('blog-home')
+            messages.success(request, f'Usuario {username} creado con exito!')
+            return redirect('login')
     else:
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm()
     return render(request, 'users/register.html',{'form':form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+
+
 
 
 
