79cf59daf019033b1868d455abe8974919ef2e87
Leandro Ruiz <leandroruiz5398@gmail.com>
2022-11-22 22:01:58 -0300

    implementacion de bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts=[
+	{
+		'autor':'Fede',
+		'titulo': 'post 1',
+		'content': 'primer post',
+		'fecha': '22 de noviembre, 2022',
+	},
+    {
+		'autor':'Luis',
+		'titulo': 'post 2',
+		'content': 'segundo post',
+		'fecha': '23 de noviembre, 2022'
+	}
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context={ 'posts':posts}
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
