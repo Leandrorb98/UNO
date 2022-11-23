02fbd6bd165bef93cd419d851df5cb7a5a9bc074
Leandro Ruiz <leandroruiz5398@gmail.com>
2022-11-23 00:48:39 -0300

    cree algunos posts para ver, correspondiente a parte 5

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title= models.CharField(max_length=100)
+    content= models.TextField()
+    fecha= models.DateTimeField(auto_now_add=True) #auto_now_add lo que hace es tomar la hora en la que fue creado el objeto y registrarlo
+    autor = models.ForeignKey(User, on_delete=models.CASCADE) #con foreignkey relaciono el usuario con el post. El argumento on_delete se usa para determinar que quiero hacer al borrar el usuario. En este caso, con CASCADE se esta determinando que cuando un usuario es eliminado, todos sus posts tambien se eliminan.
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+
+
+
+
+
+
+
+
