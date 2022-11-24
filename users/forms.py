dcb1ae5b09b4b4050b86b9d2a0aa7c83aaede605
Leandro Ruiz <leandroruiz5398@gmail.com>
2022-11-23 22:04:37 -0300

    usuarios pueden actualizar sus perfiles

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 
@@ -16,14 +17,15 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username', 'email','competicion1','competicion2', 'password1','password2']
 
 
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
 
 
-
-
-
-
-
-
-
-
-
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) #si el usuario es eliminado, el perfil tambien se elimina, con el cascade
+    image = models.ImageField(default='default.jpg', upload_to= 'profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'  # despues de crear esto hago migraciones
+
+
+
+
 
 # Create your models here.
