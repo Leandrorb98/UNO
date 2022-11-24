93900364c1c9dc3d374efa8d7e8847dfa5dabf46
Leandro Ruiz <leandroruiz5398@gmail.com>
2022-11-23 23:19:18 -0300

    ABM no esta funcionando

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import PostListView, PostDetailView#10
+from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView)                      #10
 from . import views
 
 urlpatterns = [
     #path('', views.home, name='blog-home'), #10
     path('', PostListView.as_view(), name='blog-home'), #10
-    path('', PostDetailView.as_view(), name='blog-home'), #10
+    path('post/<int:pk/', PostDetailView.as_view(), name='post-detail'), #10
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk/update/', PostUpdateView.as_view(), name='post-update'),
     path('about/', views.about, name='blog-about'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
-from django.views.generic import ListView, DetailView #10
+from django.views.generic import ListView, DetailView, CreateView #10
 
 posts=[
 	{
@@ -27,11 +28,32 @@ class PostListView(ListView):#10
 	model = Post
 	template_name = 'blog/home.html'
 	context_object_name = 'posts'
-	#ordering = ['-date_posted']
+	ordering = ['-fecha']
+	#ordering = ['-date_posted'] si no funciona, probar con fecha en vez de date_posted
 
 class PostDetailView(DetailView):#10
 	model = Post
 
+class PostCreateView(LoginRequired, MixinCreateView):#10
+	model = Post
+	fields = ['title','content']
+
+	def form_valid (self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+	 
+class PostUpdateView(LoginRequiredMixin, UpdateView):#10
+	model = Post
+	fields = ['title','content']
+	
+	def form_valid (self, form):
+	form.instance.author = self.request.user
+	return super().form_valid(form)
+
 def about(request):
     return render(request, 'blog/about.html')
+
+
+
+
 # Create your views here.
